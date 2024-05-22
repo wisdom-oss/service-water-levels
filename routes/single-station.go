e168d0b1eea6d9d4b9f60c993b89debd94fadfa6
Jan Eike Suchard <jan@suchard.cloud>
2024-05-22 15:09:35 +0200

    fixing unused local variable assignment

--- routes/single-station.go
+++ routes/single-station.go
@@ -15,11 +15,14 @@ func SingleStation(w http.ResponseWriter, r *http.Request) {
 	errorHandler := r.Context().Value(errorMiddleware.ChannelName).(chan<- interface{})
 	stationID := chi.URLParam(r, "stationID")
 	measurementStation, err := graphql.Query{}.Station(struct{ WebsiteID string }{WebsiteID: stationID})
-
+	if err != nil {
+		errorHandler <- fmt.Errorf("error while retrieving measurement station: %w", err)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	err = json.NewEncoder(w).Encode(measurementStation)
 	if err != nil {
-		errorHandler <- fmt.Errorf("error while encoding measurement stations: %w", err)
+		errorHandler <- fmt.Errorf("error while encoding measurement station: %w", err)
 		return
 	}
 }
