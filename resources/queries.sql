998da6de2740a9b0d131835b7204256e00da93cf
Jan Eike Suchard <jan@suchard.cloud>
2024-05-22 11:15:19 +0200

    now ordering measurements by date and/or station

--- resources/queries.sql
+++ resources/queries.sql
@@ -9,20 +9,24 @@ WHERE website_id = $1;
 
 -- name: get-measurements
 SELECT *
-FROM timeseries.nlwkn_water_levels;
+FROM timeseries.nlwkn_water_levels
+ORDER BY station, date;
 
 -- name: get-measurements-for-station
 SELECT *
 FROM timeseries.nlwkn_water_levels
-WHERE station = $1;
+WHERE station = $1
+ORDER BY station, date;
 
 -- name: get-measurements-in-range
 SELECT *
 FROM timeseries.nlwkn_water_levels
-WHERE date BETWEEN $1 AND $2;
+WHERE date BETWEEN $1 AND $2
+ORDER BY station, date;
 
 -- name: get-measurements-for-station-in-range
 SELECT *
 FROM timeseries.nlwkn_water_levels
 WHERE date BETWEEN $1::date AND $2::date
-AND station = $3;
+AND station = $3
+ORDER BY date;
